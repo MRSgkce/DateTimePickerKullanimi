1f0acd1a10198d6d9a7d2b98a5d4a6ceaf6813a5
MRSgkce <mursidegokce985@gmail.com>
2024-10-09 16:57:51 +0300

    tarih ve saat değerlerini seçtiğimde anlık olarak saat ve tarih teztinde görüntüledim

--- DateTimePickerKullanimi/ViewController.swift
+++ DateTimePickerKullanimi/ViewController.swift
@@ -28,10 +28,42 @@ class ViewController: UIViewController {
         
         let dokundu = UITapGestureRecognizer(target: self, action: #selector(dokunmaAlgilamaMetod))
         view.addGestureRecognizer(dokundu)
+        
+        
+        datePicker?.addTarget(self, action: #selector(tarihGoster(uiDatePicker:)), for: .valueChanged)
+        
+        timePicker?.addTarget(self, action: #selector(saatGoster(uiDatePicker:)), for: .valueChanged)
+        
+        if #available(iOS 13.4, *) {
+            //daha çok kullanılan style
+            datePicker.preferredDatePickerStyle = .wheels
+            timePicker.preferredDatePickerStyle = .wheels
+        }
     }
     //boş ekrana dokununca datrih ve saatin kapanması
     @objc func dokunmaAlgilamaMetod(){
         print("ekrana dokunuldu.")
+        
+        //sayfa üzerinden açılan herşeyi kapatır.
+        view.endEditing(true)
+        
+        
+    }
+    
+    @objc func tarihGoster(uiDatePicker:UIDatePicker){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        let tarihi = dateFormatter.string(from: uiDatePicker.date)
+        tarih.text = tarihi
+        
+    }
+    
+    @objc func saatGoster(uiDatePicker:UIDatePicker){
+        let Formatter = DateFormatter()
+        Formatter.dateFormat = "HH:mm"
+        let text = Formatter.string(from: uiDatePicker.date)
+        saat.text = text
+        
     }
 
 
