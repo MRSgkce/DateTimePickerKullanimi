5391ca71d1cb87c33b7afabf4b82c0789f13a37d
MRSgkce <mursidegokce985@gmail.com>
2024-10-09 16:26:21 +0300

    saat ve date picker ilk hal

--- DateTimePickerKullanimi/ViewController.swift
+++ DateTimePickerKullanimi/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tarih: UITextField!
+    @IBOutlet weak var saat: UITextField!
+    
+    var datePicker : UIDatePicker!
+    var timePicker : UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        datePicker=UIDatePicker()
+        datePicker.datePickerMode = .date
+        tarih.inputView=datePicker
+        
+        timePicker=UIDatePicker()
+        timePicker.datePickerMode = .time
+        saat.inputView=timePicker
     }
 
 
